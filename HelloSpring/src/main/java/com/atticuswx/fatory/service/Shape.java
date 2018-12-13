0e35487e1d79c5b7583a2e36508776144cf66282
jijie <jijie_king@163.com>
2018-12-13 14:50:53 +0800

    add factory

---
+++ HelloSpring/src/main/java/com/atticuswx/fatory/service/Shape.java
@@ -0,0 +1,13 @@
+package com.atticuswx.fatory.service;
+
+/**
+ * @Auther: XINSHIQU
+ * @Description: </br>
+ * @version: 1.0 </br>
+ * @Copyright: Copyright (c) 2015-2018 All Rights Reserved. </br>
+ * @Company: www.eaytjoys.com Inc. </br>
+ * @Date: 2018-12-13 14:43
+ */
+public interface Shape {
+    void draw();
+}
---
+++ HelloSpring/src/main/java/com/atticuswx/fatory/serviceimpl/Rectangle.java
@@ -0,0 +1,17 @@
+package com.atticuswx.fatory.serviceimpl;
+
+import com.atticuswx.fatory.service.Shape;
+
+/**
+ * @Auther: XINSHIQU
+ * @Description: </br>
+ * @version: 1.0 </br>
+ * @Copyright: Copyright (c) 2015-2018 All Rights Reserved. </br>
+ * @Company: www.eaytjoys.com Inc. </br>
+ * @Date: 2018-12-13 14:46
+ */
+public class Rectangle implements Shape {
+    public void draw() {
+
+    }
+}
